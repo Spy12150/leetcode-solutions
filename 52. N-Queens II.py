fc662d222239463b888d57d7aac823d6f2b97435
Max <yiqingwang0715@gmail.com>
2026-07-05 19:52:03 +0800

    learned set solution for nqueens

--- 52. N-Queens II.py
+++ 52. N-Queens II.py
@@ -1,5 +1,40 @@
 class Solution(object):
     def totalNQueens(self, n):
+        board = ["."*n for _ in range(n)]
+        col_set = set()
+        diag_set1 = set()
+        diag_set2 = set()
+        sol = 0
+
+        def backtrack(row):
+            nonlocal sol
+            if n == row:
+                #arc = list(board)
+                sol += 1
+                return
+
+            for x in range(n):
+                col = x
+                diag1 = row-x
+                diag2 = row+x
+                if col not in col_set and diag1 not in diag_set1 and diag2 not in diag_set2:
+                    board[row] = "." * col + "Q" + "." * (n-col-1)
+                    col_set.add(col)
+                    diag_set1.add(diag1)
+                    diag_set2.add(diag2)
+                    backtrack(row+1)
+                    board[row] = "."*n
+                    col_set.remove(col)
+                    diag_set1.remove(diag1)
+                    diag_set2.remove(diag2)
+
+        backtrack(0)
+        return sol
+                
+# Worse original solution I did
+
+"""class Solution(object):
+    def totalNQueens(self, n):
         board = ["." *n for _ in range(n)]
         results = [0]
 
@@ -25,5 +60,6 @@ class Solution(object):
             return True
 
         backtrack(0)
-        return results[0]
+        return results[0]"""
         
+
